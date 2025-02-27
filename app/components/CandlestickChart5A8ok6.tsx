d17aeded7e489bf6e704e566f59b7f35ff0cb0cf
skylar-Youn <sky_tech@naver.com>
2025-02-27 13:27:20 +0900

    40EMA=>60EMA가 240EMA 돌파시 매수 조건 변경

--- app/components/CandlestickChart5A8ok6.tsx
+++ app/components/CandlestickChart5A8ok6.tsx
@@ -353,9 +353,9 @@ const THRESHOLD_ANGLE_240_MINUS = -MIN_SLOPE_THRESHOLD;
       // 매수 추가 조건 확인
       const buyAdditionalCondition = 
         ma240Index >= 0 && 
-        ma120Index >= 0 && 
-        fortyEMA[i].value < ma240Data[ma240Index].value && 
-        fortyEMA[i].value > ma120Data[ma120Index].value;
+        ma240Index > 0 && // 이전 데이터가 있는지 확인
+        sixtyEMA[i].value > ma240Data[ma240Index].value && // 현재 60EMA가 240EMA보다 위에 있음
+        sixtyEMA[i-1].value <= ma240Data[ma240Index-1].value; // 이전에는 60EMA가 240EMA보다 아래에 있었음
       
       // 매도 추가 조건 확인 수정 (60EMA가 240EMA를 하향 돌파)
       const sellAdditionalCondition = 
