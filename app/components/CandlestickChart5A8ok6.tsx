b1274f659d2adc3adc128c68cb06ec0c19351d26
skylar-Youn <sky_tech@naver.com>
2025-03-01 11:46:03 +0900

    60MA ->240MA가 360MA 하방돌파시 매도

--- app/components/CandlestickChart5A8ok6.tsx
+++ app/components/CandlestickChart5A8ok6.tsx
@@ -368,12 +368,12 @@ const THRESHOLD_ANGLE_240_MINUS = -MIN_SLOPE_THRESHOLD;
         sixtyEMA[i].value > ma240Data[ma240Index].value && // 현재 60EMA가 240EMA보다 위에 있음
         sixtyEMA[i-1].value <= ma240Data[ma240Index-1].value; // 이전에는 60EMA가 240EMA보다 아래에 있었음
       
-      // 매도 추가 조건 확인 수정 (60EMA가 360EMA를 하향 돌파)
+      // 매도 추가 조건 확인 수정 (240EMA가 360EMA를 하향 돌파)
       const sellAdditionalCondition = 
         ma360Index >= 0 && 
         ma360Index > 0 && // 이전 데이터가 있는지 확인
-        sixtyEMA[i].value < ma360Data[ma360Index].value && // 현재 60EMA가 360EMA보다 아래
-        sixtyEMA[i-1].value >= ma360Data[ma360Index-1].value; // 이전에는 60EMA가 360EMA보다 위에 있었음
+        twoFortyEMA[i].value < ma360Data[ma360Index].value && // 현재 240EMA가 360EMA보다 아래
+        twoFortyEMA[i-1].value >= ma360Data[ma360Index-1].value; // 이전에는 240EMA가 360EMA보다 위에 있었음
       
       // 매수 조건 지속 시간 추적
       if (buyBaseCondition) {
